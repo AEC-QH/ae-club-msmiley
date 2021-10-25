88805200475d232e169574ebf227373841451f1b
Ubuntu <ubuntu@ip-172-31-30-239.us-east-2.compute.internal>
2021-10-25 18:13:05 +0000

    updates based on review

--- customers.sql
+++ customers.sql
@@ -4,9 +4,9 @@ SELECT customers.id AS customer_id
        , min(orders.created_at)    AS first_order_at
        , NVL(count(orders.id),0)   AS number_orders
        , max(orders.created_at)    AS last_order_at
-FROM coffee_shop.customers a
-LEFT JOIN coffee_shop.orders b
-ON a.id = b.customer_id
+FROM coffee_shop.customers
+LEFT JOIN coffee_shop.orders
+ON customers.id = orders.customer_id
 GROUP BY 1,2,3
 ORDER BY first_order_at
 ;
